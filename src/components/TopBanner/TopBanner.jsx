a7639ecda5a6f6fb30f7fb153654a10fad9c0570
unknown <sarojsardar25@gmail.com>
2024-01-09 14:18:29 +0545

    Banner Section Final

--- src/components/TopBanner/TopBanner.jsx
+++ src/components/TopBanner/TopBanner.jsx
@@ -1,18 +1,29 @@
-import React, { Component, Fragment } from 'react'
-import { Container } from 'react-bootstrap'
-import '../../assets/css/custom.css'
+import React, { Component, Fragment } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import Button from 'react-bootstrap/Button';
+import "../../assets/css/bootstrap.min.css";
+import "../../assets/css/custom.css";
 
 export class TopBanner extends Component {
   render() {
     return (
       <Fragment>
-        <Container className='topFixedBanner'>
-
+        <Container fluid={true} className="topFixedBanner p-0">
+          <div className="topBannerOverlay">
+            <Container className="sliderContent">
+              <Row>
+                <Col className="text-center">
+                <h1 className="sliderTitle">Hello World!</h1>
+                <h4 className="sliderSubtitle">Learn Professionally</h4>
+                <Button variant="primary">Read More</Button>
+                </Col>
+              </Row>
+            </Container>
+          </div>
         </Container>
-        
       </Fragment>
-    )
+    );
   }
 }
 
-export default TopBanner
+export default TopBanner;
